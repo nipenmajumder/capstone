3e8093f937af528195e4f3127fabd5f0bf60a782
Nipen Majumder <amitmojumder356@gmail.com>
2024-11-29 18:52:22 +0600

    Map Customizations

--- resources/views/collection/index.blade.php
+++ resources/views/collection/index.blade.php
@@ -7,13 +7,15 @@
             crossorigin=""></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css"/>
     <script src="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.fullscreen@1.6.0/Control.FullScreen.css"/>
+    <script src="https://unpkg.com/leaflet.fullscreen@1.6.0/Control.FullScreen.js"></script>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="text-gray-900">
                     <div class="max-w-7xl mx-auto">
-                        <div id="map" style="width: 1000px; height: 600px;"></div>
+                        <div id="map" style="width: 1300px;height: 1200px"></div>
                     </div>
                 </div>
             </div>
@@ -25,16 +27,17 @@
         }
 
         $dustbins = [];
-        for ($i = 1; $i <= 5; $i++) {
+        for ($i = 1; $i <= 20; $i++) {
             $dustbins[] = [
                 'id' => $i,
                 'latitude' => getRandomCoordinate(23.7000, 23.9000),
-                'longitude' => getRandomCoordinate(90.3000, 90.5000)
+                'longitude' => getRandomCoordinate(90.3000, 90.5000),
+                'filled' => mt_rand(0, 100),
             ];
         }
     @endphp
     <script>
-        let map, userLocation, routingControl;
+        let map, userLocation, routingControl, fillLevel, iconColor, icon, dustbinLocation, distance;
 
         initializeMap();
         getUserLocation();
@@ -46,13 +49,7 @@
                 maxZoom: 19,
                 attribution: '&copy; OpenStreetMap'
             }).addTo(map);
-
-            map.on('click', e => {
-                L.popup()
-                    .setLatLng(e.latlng)
-                    .setContent(`You clicked the map at ${e.latlng.toString()}`)
-                    .openOn(map);
-            });
+            L.control.fullscreen().addTo(map);
         }
 
         function getUserLocation() {
@@ -85,26 +82,39 @@
 
         function markDustbins() {
             @foreach($dustbins as $dustbin)
+                fillLevel = {{ $dustbin['filled'] }};
+             dustbinLocation = L.latLng({{ $dustbin['latitude'] }}, {{ $dustbin['longitude'] }});
+             distance = userLocation ? userLocation.distanceTo(dustbinLocation) : 'unknown';
+
             L.marker([{{ $dustbin['latitude'] }}, {{ $dustbin['longitude'] }}]).addTo(map)
-                .bindPopup(`Dustbin ID: {{ $dustbin['id'] }}`)
-                .on('popupopen', () => showRoute(L.latLng({{ $dustbin['latitude'] }}, {{ $dustbin['longitude'] }})));
+                .bindPopup(`
+                Dustbin ID: {{ $dustbin['id'] }}<br>
+                Filled: {{ $dustbin['filled'] }}%<br>
+                Distance: ${distance} meters<br>
+                <button class="text-white bg-green-700  font-medium rounded text-xs px-1 py-1 text-center" onclick="showRouteToDustbin(${dustbinLocation.lat}, ${dustbinLocation.lng})">Show Route</button>
+            `);
             @endforeach
-    }
-
-    function showRoute(destination) {
-        if (!userLocation) {
-            console.log("User location not available.");
-            return;
         }
 
-        if (routingControl) {
-            map.removeControl(routingControl);
+        function showRouteToDustbin(lat, lng) {
+            const destination = L.latLng(lat, lng);
+            showRoute(destination);
         }
 
-        routingControl = L.Routing.control({
-            waypoints: [userLocation, destination],
-            routeWhileDragging: true
-        }).addTo(map);
-    }
-</script>
+        function showRoute(destination) {
+            if (!userLocation) {
+                console.log("User location not available.");
+                return;
+            }
+
+            if (routingControl) {
+                map.removeControl(routingControl);
+            }
+
+            routingControl = L.Routing.control({
+                waypoints: [userLocation, destination],
+                routeWhileDragging: true
+            }).addTo(map);
+        }
+    </script>
 </x-app-layout>
